9694571ad04a467695e80a2e0b30e82e790bd1dd
vl <gztrk@engineer.com>
2024-12-15 23:08:53 +0300

    add loop and windows close check

--- main.py
+++ main.py
@@ -8,6 +8,15 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+    
+    running = True
+    while running: 
+        # check if the user has closed the window and exit the game loop if they do. It will make the window's close button work.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
